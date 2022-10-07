3a66922c3fd2d883a819346b8245142e2dcb7235
notbozho <bozhoivanow@gmail.com>
2022-10-08 01:13:00 +0300

    getting basic info from backend (just testing)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/v1/users').then(response => {
+      
+      setUsers(response.data)
+    }).catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <h1>hi</h1>
+      {users.map(u => <h1>{u.firstName}</h1>)}
     </div>
   );
 }
